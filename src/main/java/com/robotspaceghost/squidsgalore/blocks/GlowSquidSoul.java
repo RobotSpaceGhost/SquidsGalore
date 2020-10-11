8758d77a6e4e24bd146b6af720b48733c6e610a3
RobotSpaceGhost <RobotSpaceGhost@gmail.com>
2020-10-11 14:10:45 -0400

    Changed entity based glow soul, now the entity that spawns a soul will keep track of its location

--- src/main/java/com/robotspaceghost/squidsgalore/blocks/GlowSquidSoul.java
+++ src/main/java/com/robotspaceghost/squidsgalore/blocks/GlowSquidSoul.java
@@ -1,5 +1,4 @@
 package com.robotspaceghost.squidsgalore.blocks;
-import com.robotspaceghost.squidsgalore.init.ModBlocks;
 import net.minecraft.block.*;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
@@ -16,9 +15,8 @@ import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
 import javax.annotation.Nullable;
-import java.util.Random;
+
 
 public class GlowSquidSoul extends Block implements IWaterLoggable {
     public static final BooleanProperty LIT = RedstoneTorchBlock.LIT;
@@ -75,16 +73,6 @@ public class GlowSquidSoul extends Block implements IWaterLoggable {
         return IWaterLoggable.super.canContainFluid(worldIn, pos, state, fluidIn);
     }
 
-    /*
-    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
-        if (!worldIn.isRemote) {
-            boolean wet = state.get(WATERLOGGED);
-            if (neighborIsGlowSquidSoul(worldIn,pos)) {
-                clearGlowSquidSoul(worldIn, pos, blockIn, wet);
-            }
-        }
-    }*/
-
     public void clearGlowSquidSoul(World worldIn, BlockPos pos, boolean wet){
         if (wet){
             worldIn.setBlockState(pos, Blocks.WATER.getDefaultState());
@@ -93,54 +81,13 @@ public class GlowSquidSoul extends Block implements IWaterLoggable {
         }
     }
 
-    public void clearSurroundingSouls(World worldIn, BlockPos pos, boolean clearCenter){
-        for (int i = -1; i < 2; i++) {
-            for (int j = -1; j < 2; j++) {
-                for (int k = -1; k < 2; k++){
-                    BlockPos newPos = new BlockPos(pos.offset(Direction.EAST, i).offset(Direction.UP, j).offset(Direction.NORTH, k));
-                    BlockState newState = worldIn.getBlockState(newPos);
-                    Block newBlock = worldIn.getBlockState(newPos).getBlock();
-                    if (!clearCenter && (i == 0) && (j == 0) && (k == 0)){
-                        System.out.println(i + ", " + j + ", " + k);
-                        System.out.println("center of soul found!");
-                    }
-                    else if (newBlock instanceof GlowSquidSoul){
-                        ((GlowSquidSoul) newBlock).clearGlowSquidSoul(worldIn, newPos, ((GlowSquidSoul) newBlock).isWet(newState));
-                    }
-                }
-            }
-        }
-    }
-    /*
-    public boolean neighborIsGlowSquidSoul(World worldIn, BlockPos pos){
-        BlockState centerState = worldIn.getBlockState(pos).getBlock().getDefaultState();
-        if (centerState != ModBlocks.GLOW_SQUID_SOUL.get().getDefaultState()) return false; //just in case?
-        int uFlag = (worldIn.getBlockState(pos.offset(Direction.UP, 1)).getBlock().getDefaultState() == centerState) ? 1 : 0;
-        int dFlag = (worldIn.getBlockState(pos.offset(Direction.DOWN, 1)).getBlock().getDefaultState() == centerState) ? 1 : 0;
-        int eFlag = (worldIn.getBlockState(pos.offset(Direction.EAST, 1)).getBlock().getDefaultState() == centerState) ? 1 : 0;
-        int wFlag = (worldIn.getBlockState(pos.offset(Direction.WEST, 1)).getBlock().getDefaultState() == centerState) ? 1 : 0;
-        int nFlag = (worldIn.getBlockState(pos.offset(Direction.NORTH, 1)).getBlock().getDefaultState() == centerState) ? 1 : 0;
-        int sFlag = (worldIn.getBlockState(pos.offset(Direction.SOUTH, 1)).getBlock().getDefaultState() == centerState)  ? 1 : 0;
-        return ((uFlag + dFlag + eFlag + wFlag + nFlag + sFlag) >= 1);
-    } */
     public boolean isReplaceable(BlockState state, BlockItemUseContext useContext) {
         ItemStack itemstack = useContext.getItem();
-        return useContext.replacingClickedOnBlock() && itemstack.getItem() != this.asItem();
+        return useContext.replacingClickedOnBlock() && (itemstack.getItem() != this.asItem());
     }
-    /*
-    public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
-        boolean wet = state.get(WATERLOGGED);
-        Block blockIn = worldIn.getBlockState(pos).getBlock();
-        if (neighborIsGlowSquidSoul(worldIn,pos)) {
-            clearGlowSquidSoul(worldIn, pos, blockIn, wet);
-        }
-    } */
 
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
         builder.add(LIT, WATERLOGGED);
-    }
-    public boolean isWet(BlockState state){
-        return state.get(WATERLOGGED);
     }
     public boolean isTransparent(BlockState state) {
         return true;
--- src/main/java/com/robotspaceghost/squidsgalore/entities/BabyKrakenEntity.java
+++ src/main/java/com/robotspaceghost/squidsgalore/entities/BabyKrakenEntity.java
@@ -11,17 +11,26 @@ import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import javax.annotation.Nullable;
+import java.util.List;
 
 
 public class BabyKrakenEntity extends AnimalEntity {
+    /*
+    todo: stareAtFlowerGoal, sitOnBedGoal make tameable, belong to spawning player, if killed by player semiperm debuff "Omen of the Seas"
+    safg: stops for a moment to stare at flowers, like it stares at players
+    sobg: like cats
+    ofts: upon entering ocean biome, summon boss fight
 
+    */
     public static final Ingredient TEMPTATION_ITEMS = Ingredient.fromItems(ModItems.INK_ON_A_STICK.get());
 
     public BabyKrakenEntity(EntityType<? extends AnimalEntity> type, World worldIn) {
@@ -41,10 +50,11 @@ public class BabyKrakenEntity extends AnimalEntity {
         super.registerGoals();
         this.goalSelector.addGoal(0, new SwimGoal(this));
         this.goalSelector.addGoal(1, new PanicGoal(this,1.25D));
-        this.goalSelector.addGoal(2, new TemptGoal(this, 1.1D, TEMPTATION_ITEMS, false));
+        this.goalSelector.addGoal(2, new TemptGoal(this, 1.3D, TEMPTATION_ITEMS, false));
         this.goalSelector.addGoal(3, new WaterAvoidingRandomWalkingGoal(this,1.0D));
         this.goalSelector.addGoal(4, new LookAtGoal(this, PlayerEntity.class, 10.0f));
         this.goalSelector.addGoal(5, new LookRandomlyGoal(this));
+        //this.goalSelector.addGoal(6, new );
     }
 
     @Override
@@ -81,20 +91,32 @@ public class BabyKrakenEntity extends AnimalEntity {
     start glowsquid func
     ----------------------
      */
+    //public List entities;
+    //public AxisAlignedBB aura;
+    public BlockPos soulLoc = new BlockPos(0,250,0);
+
     @Override
     public void livingTick() {
         super.livingTick();
+
         if (this.isAlive()) {
-            BlockPos soulLoc = new BlockPos(this.getPosX(), this.getPosY() + 1, this.getPosZ());
+            BlockPos newSoulLoc = new BlockPos(this.getPosX(), this.getPosY(), this.getPosZ());
             World worldIn = this.getEntityWorld();
-            if (worldIn.getBlockState(soulLoc).getBlock().getDefaultState() != ModBlocks.GLOW_SQUID_SOUL.get().getDefaultState()) {
+            /*
+            aura = new AxisAlignedBB(this.getPosX() - 1, this.getPosY() - 1, this.getPosZ() - 1, this.getPosX() + 2, this.getPosY() + 2, this.getPosZ() + 2);
+            entities = world.getEntitiesWithinAABB(getClass(), aura);
+
+            if (!entities.isEmpty() && (entities.size() >= 2)) {
+                System.out.println("These Entities Are within Range!" + entities + "");
+            }*/
+            if ((soulLoc.getX() != newSoulLoc.getX()) || (soulLoc.getY() != newSoulLoc.getY()) || (soulLoc.getZ() != newSoulLoc.getZ())){
+                if (worldIn.getBlockState(soulLoc).getBlock() instanceof GlowSquidSoul) {
+                        ((GlowSquidSoul) worldIn.getBlockState(soulLoc).getBlock()).clearGlowSquidSoul(worldIn,soulLoc,(worldIn.getFluidState(soulLoc).getFluid() == Fluids.WATER));
+                }
+                soulLoc = new BlockPos(newSoulLoc); //change soul location
                 if ((worldIn.getBlockState(soulLoc).getBlock().getDefaultState() == Blocks.AIR.getDefaultState()) ||
-                        (worldIn.getBlockState(soulLoc).getBlock().getDefaultState() == Blocks.WATER.getDefaultState())) {
+                   (worldIn.getBlockState(soulLoc).getBlock().getDefaultState() == Blocks.WATER.getDefaultState())){
                     worldIn.setBlockState(soulLoc, ModBlocks.GLOW_SQUID_SOUL.get().getDefaultState());
-                    Block newBlock = worldIn.getBlockState(soulLoc).getBlock();
-                    if (newBlock instanceof GlowSquidSoul) {
-                        ((GlowSquidSoul) newBlock).clearSurroundingSouls(worldIn, soulLoc, false);
-                    }
                 }
             }
         }
@@ -102,11 +124,9 @@ public class BabyKrakenEntity extends AnimalEntity {
     @Override
     public void onDeath(DamageSource cause) {
         super.onDeath(cause);
-        BlockPos soulLoc = new BlockPos(this.getPosX(), this.getPosY() + 1, this.getPosZ());
         World worldIn = this.getEntityWorld();
-        Block soulBlock = worldIn.getBlockState(soulLoc).getBlock();
-        if (soulBlock instanceof GlowSquidSoul) {
-            ((GlowSquidSoul) soulBlock).clearSurroundingSouls(worldIn, soulLoc, true);
+        if (worldIn.getBlockState(soulLoc).getBlock() instanceof GlowSquidSoul) {
+            ((GlowSquidSoul) worldIn.getBlockState(soulLoc).getBlock()).clearGlowSquidSoul(worldIn,soulLoc,(worldIn.getFluidState(soulLoc).getFluid() == Fluids.WATER));
         }
     }
 /*
