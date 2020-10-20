6c861d51569fac00735c19dcb34cf8f0b0522aa2
RobotSpaceGhost <RobotSpaceGhost@gmail.com>
2020-10-20 19:41:11 -0400

    started working on potion effect events

--- src/main/java/com/robotspaceghost/squidsgalore/init/ModEffects.java
+++ src/main/java/com/robotspaceghost/squidsgalore/init/ModEffects.java
@@ -1,11 +1,23 @@
 package com.robotspaceghost.squidsgalore.init;
 
 import com.robotspaceghost.squidsgalore.SquidsGalore;
-import net.minecraft.potion.Effect;
-import net.minecraft.potion.EffectType;
+import com.robotspaceghost.squidsgalore.items.SquidMilk.SquidInkItem;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.ElderGuardianEntity;
+import net.minecraft.entity.monster.GuardianEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PotionEntity;
+import net.minecraft.entity.projectile.ThrowableEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.*;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.Explosion;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.ProjectileImpactEvent;
+import net.minecraftforge.event.entity.living.PotionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -76,6 +88,44 @@ public class ModEffects {
                     ModEffects.KRAKEN_BREATH_EFFECT,
                     ModEffects.OMEN_OF_THE_SEAS
             );
+        }
+    }
+    @Mod.EventBusSubscriber(modid = SquidsGalore.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE,  value = Dist.CLIENT)
+    public static class ModEffectEvents {
+        @SubscribeEvent
+        public static void onPotionAdded(PotionEvent.PotionAddedEvent event){
+            if (!event.getEntity().world.isRemote) {
+                LivingEntity targetEntity = event.getEntityLiving();
+                EffectInstance potionEffect = event.getPotionEffect();
+                if (potionEffect.getPotion() == ModEffects.SQUID_INK_EFFECT) {
+                    targetEntity.addPotionEffect(new EffectInstance(Effects.BLINDNESS, potionEffect.getDuration(), potionEffect.getAmplifier()));
+                }
+                if (potionEffect.getPotion() == ModEffects.KRAKEN_BREATH_EFFECT) {
+                    if (targetEntity instanceof ElderGuardianEntity)
+                        targetEntity.addPotionEffect(new EffectInstance(Effects.INSTANT_DAMAGE, 1, potionEffect.getAmplifier() + 2));
+                    else if (targetEntity instanceof GuardianEntity)
+                        targetEntity.addPotionEffect(new EffectInstance(Effects.INSTANT_DAMAGE, 1, potionEffect.getAmplifier() + 3));
+                    else {
+                        targetEntity.addPotionEffect(new EffectInstance(Effects.CONDUIT_POWER, potionEffect.getDuration(), potionEffect.getAmplifier()));
+                        targetEntity.addPotionEffect(new EffectInstance(Effects.DOLPHINS_GRACE, potionEffect.getDuration(), potionEffect.getAmplifier()));
+                    }
+                }
+            }
+        }
+
+        @SubscribeEvent
+        public static void onPotionImpact(ProjectileImpactEvent.Throwable event){
+            World worldIn = event.getThrowable().world;
+            if (!worldIn.isRemote) {
+                ThrowableEntity thrownObject = event.getThrowable();
+                Potion potion = (thrownObject instanceof PotionEntity) ? PotionUtils.getPotionFromItem(((PotionEntity) thrownObject).getItem()) : null;
+                EffectInstance potionEI = (potion != null && !potion.getEffects().isEmpty()) ? potion.getEffects().get(0) : null;
+                if (potionEI != null) {
+//                    if (potionEI.getPotion() == ModEffects.NITRO_EFFECT) {
+//                        worldIn.createExplosion(thrownObject,thrownObject.getPosX(),thrownObject.getPosY(),thrownObject.getPosZ(),2.5f, Explosion.Mode.BREAK);
+//                    }
+                }
+            }
         }
     }
 }
